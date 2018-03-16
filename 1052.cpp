42fabd30ea7a1349ac721f2ed0159fea1f152712
forestCai <2801152588@qq.com>
2018-03-16 18:59:32 +0800

    第二次编写
    主要是采用了链表处理的通法

--- 1052.cpp
+++ 1052.cpp
@@ -1,3 +1,4 @@
+/*
 #include<cstdio>
 #include<algorithm>
 using namespace std;
@@ -55,4 +56,48 @@ int main(){
     else
         printf("-1");
   }
+}
+*/
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int MAXI = 100000;
+int s[MAXI] ;
+int aa[MAXI][2];
+
+bool qq(int a,int b){
+  return aa[a][0] < aa[b][0];
+}
+int main(){
+   int num ;
+   int fir;
+   int i = 0;
+   int a,b,c;
+   scanf("%d %d",&num,&fir);
+   for(i = 0;i < num;i++){
+     scanf("%d %d %d",&a,&b,&c);
+     aa[a][0] = b;
+     aa[a][1] = c;
+   }
+   //printf("read finished\n");
+   int r = fir;
+   i = 0;
+   while(r != -1){
+     s[i ++] = r;
+     r = aa[r][1];
+   }
+   //printf("will sort\n");
+   sort(s,s + i ,qq);
+   printf("%d %05d\n",i,s[0]);
+   int  j = 0;
+   int flg = 0;
+   for(j = 0;j < i;j++){
+     if(flg == 0){
+       printf("%05d %d ",s[j],aa[s[j]][0]);
+       flg = 1;
+     }
+     else
+       printf("%05d\n%05d %d ",s[j],s[j],aa[s[j]][0]);
+   }
+   printf("-1");
 }
