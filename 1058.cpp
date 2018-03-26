552491870746b20c85f69ed43fc4b2bd7da1f729
forestCai <2801152588@qq.com>
2018-03-26 20:29:23 +0800

    第二次编写
    满分完成

--- 1058.cpp
+++ 1058.cpp
@@ -1,4 +1,4 @@
-#include<cstdio>
+/*#include<cstdio>
 int main(){
 
 int n[6]={0};
@@ -17,4 +17,27 @@ if(n[1]>17){
   n[0]+=1;
 }
 printf("%d.%d.%d",n[0],n[1],n[2]);
+}
+*/
+#include<cstdio>
+
+
+int main(){
+  int a1,a2,a3,b1,b2,b3;
+  scanf("%d.%d.%d %d.%d.%d",&a1,&a2,&a3,&b1,&b2,&b3);
+  int k1 = 0,k2 = 0;
+  if(a3 + b3 > 28){
+     k1 = 1;
+     a3 = a3 + b3 - 29;
+  }else{
+     a3 = a3 + b3;
+  }
+  if(b2 + a2 + k1> 16){
+    k2 = 1;
+    a2 = b2 + a2 - 17 + k1;
+  }else{
+    a2 = b2 + a2 + k1;
+  }
+  printf("%d.%d.%d",a1 + b1 + k2,a2,a3);
+
 }
