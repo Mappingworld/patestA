69ee5282f7b828a74f67da5a294707ed59d5bc5d
forestCai <2801152588@qq.com>
2018-03-27 21:35:57 +0800

    第二次编写
    修复了没有通过的测试点，关于之前对long long 型的理解，%lld

--- 1081.cpp
+++ 1081.cpp
@@ -2,6 +2,7 @@
 //其中数组使用long long 建立二位数时候，无法使用
 //有一个测试点迟迟通不过，前面考虑可能是因为负数问题后来发现，似乎是在每次通分后的累计没有及时约分导致的感觉这个有点恐怖
 //记录写错
+/*
 #include<cstdio>
 
 int gcd(int a,int b){//求解最大公约数
@@ -60,4 +61,68 @@ int main(){
        printf("0");
      }
    }
+}
+*/
+#include<cstdio>
+
+long long a = 0,b = 0;
+
+long gcd(long v,long c){//求解最大公约数
+ if(c == 0)
+ return v;
+ else
+ return gcd(c,v % c);
+
+}
+void sumR(long c,long d){
+  //找出最大公因数
+ // printf("NOW %d %d u\n",c,d);
+  long long m = d,n = b;
+  n = gcd(m,n);
+
+  //此时n为最大公因数
+ // printf("n = %d  ",n);
+  long long r = d / n;
+  long long rr = r * b;
+//  printf("c = %d r = %d",c,r);
+  long long r1 = rr / d;
+   long long c1 = c * r1;
+ // printf("a = %d rr = %d d = %d",a,rr,b);
+ long long r2 = rr / b;
+  long long c2 = a * r2;
+ // printf("c1 = %d c2 = %d\n",c1,c2);
+  a = c1 + c2;
+  b = rr;
+ // printf("a = %d b = %d",a,b);
+}
+
+int main(){
+   int num = 0;
+   scanf("%d",&num);
+    int i;
+    long long c,d;
+   scanf("%lld/%lld",&a,&b);
+   for(i = 1;i < num;i++){
+     scanf("%lld/%lld",&c,&d);
+     //printf("OOK\n");
+     sumR(c,d);
+   }
+   if(a < 0){
+    printf("-");
+    a = -a;
+   }
+    long long n = gcd(a,b);
+  // printf("n = %d ",n);
+   if(a > b && a % b / n != 0){
+    printf("%lld %lld/%lld",a / b,a % b / n,b / n);
+    return 0;
+   }
+    if(a > b && a % b / n == 0){
+    printf("%lld",a / b);
+    return 0;
+   }
+   if(a != 0)
+    printf("%lld/%lld",a / n,b / n);
+   if(a == 0)
+    printf("0");
 }
